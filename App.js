81fe98d331ccff3134bed458cfb9fdc19b54d700
Prash0265 <kmrprashanth92@gmail.com>
2023-12-30 16:38:05 -0500

    installed ract packages and changed code

--- App.js
+++ App.js
@@ -16,11 +16,16 @@
 // console.log('test :>> ');
 // console.log(root.render(heading));
 
+import React from "react";
+import { createRoot } from 'react-dom';
+
+
+
 const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" },
     [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]),
 React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm h1 tag"),
 React.createElement("h2", {}, "I'm h2 tag")])]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(parent);
 console.log('test :>> ');
 console.log(root.render(parent));
