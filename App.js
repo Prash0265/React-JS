0c21fda595ba2cd9542b4ff5cb168e218513810f
Prash0265 <kmrprashanth92@gmail.com>
2023-12-30 18:33:45 -0500

    production ready build

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-{/* <div id="parent">
+/* <div id="parent">
     <div id="child1">
         <h1> I'm h1 tag</h1>
         <h2> I'm h2 tag</h2>
@@ -7,7 +7,7 @@
         <h1> I'm h1 tag</h1>
         <h2> I'm h2 tag</h2>
     </div>
-</div> */}
+</div> */
 
 
 // const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
@@ -17,12 +17,12 @@
 // console.log(root.render(heading));
 
 import React from "react";
-import { createRoot } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 
 
 const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" },
-    [React.createElement("h1", {}, "I'm h1 tag"), React.createElement("h2", {}, "I'm h2 tag")]),
+    [React.createElement("h1", {}, "I'm Prashanth"), React.createElement("h2", {}, "I'm h2 tag")]),
 React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm h1 tag"),
 React.createElement("h2", {}, "I'm h2 tag")])]);
 const root = createRoot(document.getElementById("root"));
