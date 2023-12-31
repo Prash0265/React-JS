7af7b2b4932576f2fd9b0af6a008e7d943e1a30b
Prash0265 <kmrprashanth92@gmail.com>
2023-12-30 22:59:51 -0500

    prettier modified

--- Ep2.js
+++ Ep2.js
@@ -9,7 +9,6 @@
     </div>
 </div> */
 
-
 // const heading = React.createElement("h1", {id:"heading"}, "Hello world from react");
 // const root = ReactDOM.createRoot(document.getElementById("root"));
 // root.render(heading);
@@ -17,15 +16,19 @@
 // console.log(root.render(heading));
 
 import React from "react";
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
-
-
-const parent = React.createElement("div", { id: "parent" }, [React.createElement("div", { id: "child1" },
-    [React.createElement("h1", {}, "I'm Prashanth"), React.createElement("h2", {}, "I'm h2 tag")]),
-React.createElement("div", { id: "child2" }, [React.createElement("h1", {}, "I'm h1 tag"),
-React.createElement("h2", {}, "I'm h2 tag")])]);
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", {}, "I'm Prashanth"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm h1 tag"),
+    React.createElement("h2", {}, "I'm h2 tag"),
+  ]),
+]);
 const root = createRoot(document.getElementById("root"));
 root.render(parent);
-console.log('test :>> ');
+console.log("test :>> ");
 console.log(root.render(parent));
--- Ep3.js
+++ Ep3.js
@@ -1,16 +1,12 @@
 import React from "react";
 
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 
 // React Element
 
-const parent = React.createElement(
-    "h1",
-    { id: "heading" },
-    "Test");
+const parent = React.createElement("h1", { id: "heading" }, "Test");
 const root = createRoot(document.getElementById("root"));
-const jsxheading = <h1 id="heading">Test1</h1>;
+const jsxheading = <h1 id="heading">Test11</h1>;
+console.log("parent :>> ", parent);
+console.log("jsxheading :>> ", jsxheading);
 root.render(jsxheading);
-
-console.log('parent :>> ', parent);
-console.log('jsxheading :>> ', jsxheading);
