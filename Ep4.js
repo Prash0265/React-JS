ee16accc3c91b678a71c418fb9ffe12b758d1f97
Prash0265 <kmrprashanth92@gmail.com>
2024-01-03 06:16:43 -0500

    props

--- Ep4.js
+++ Ep4.js
@@ -28,21 +28,39 @@ const Header = () => {
   );
 };
 
-const Restaurantcard = () => {
-    return (
-        <div className="res-card" >
-            <h3>Meghana foods</h3>
-
-        </div>
-    )
-}
+const Restaurantcard = (props) => {
+const {resName,cuisine,rating,deliverytime} = props;
+  return (
+    <div className="res-card" style={{ backgroundColor: "#f0f0f0" }}>
+      <img
+        className="res-logo"
+        src="https://www.licious.in/blog/wp-content/uploads/2020/12/Hyderabadi-chicken-Biryani.jpg"
+      ></img>
+      <h3>{resName}</h3>
+      <h4>{cuisine}</h4>
+      <h4>{rating}</h4>
+      <h4>{deliverytime}</h4>
+    </div>
+  );
+};
 
 const Body = () => {
   return (
     <div className="body">
-      <div className="Search">Search</div>
+      <div className="search">Search</div>
       <div className="res-container">
-        <Restaurantcard/>
+        <Restaurantcard
+          resName="KFC"
+          cuisine="Fried chicken and wings"
+          rating="4.2 stars"
+          deliverytime="20 minutes"
+        />
+        <Restaurantcard
+          resName="Burger king"
+          cuisine="Burger and chicken nuggets"
+          rating="4.4 stars"
+          deliverytime="25 minutes"
+        />
       </div>
     </div>
   );
@@ -52,7 +70,7 @@ const AppLayout = () => {
   return (
     <div className="app">
       <Header />
-      <Body/>
+      <Body />
     </div>
   );
 };
