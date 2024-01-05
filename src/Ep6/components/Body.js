339f3fc25677f4e1eb43fa783298175677b9c188
Prash0265 <kmrprashanth92@gmail.com>
2024-01-05 04:16:40 -0500

    Added shimmer UI

--- src/Ep6/components/Body.js
+++ src/Ep6/components/Body.js
@@ -1,26 +1,35 @@
 import Restaurantcard from "./RestaurantCard";
 
 import { useEffect, useState } from "react";
-
-
+import Shimmer from "./Shimmer";
 
 const Body = () => {
-
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   useEffect(() => {
     console.log("useeffect called");
     fetchData();
   }, []);
 
-  const fetchData = async () =>{
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-  
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.406498&lng=78.47724389999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
     const json = await data.json();
     console.log(json);
-    setListOfRestaurants(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants);
+    setListOfRestaurants(
+      json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
   };
 
-  return (
+  //conditional rendering
+  // if (listOfRestaurants.length === 0) {
+  //   return <Shimmer/>;
+  // }
+
+  return listOfRestaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
         <button
